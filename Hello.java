ccc2b1e125cf9527687f6d62b79cda13468e2d25
Gayasri72 <gayasripethum1@gmail.com>
2024-10-06 16:58:55 +0530

    Day-02

--- Hello.java
+++ Hello.java
@@ -3,5 +3,25 @@ class Hello
     public static void main(String a[])
     {
         System.out.println("Hello world!!");
+        // different between print and println
+
+        System.out.println(3+5);
+        System.out.println(8+5);
+
+        System.out.print(3+5);
+        System.out.print(8+5);
+
+        // variables
+
+        int num = 10;
+        int num1 = 20;
+        int num2 = 30;
+        int result = num1+num2;
+
+        System.out.println(num);
+        System.out.println(num1+num2);
+        System.out.println(result);
+        
+
     }
 }
